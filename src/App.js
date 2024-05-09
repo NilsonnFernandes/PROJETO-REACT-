8333911f34ef5f5eb1333dc8a5c7c7b5b6e267ff
NilsonnFernandes <nilnen@hotmail.com>
2024-05-08 22:37:08 -0300

    criação primeiro projeto Julio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import Botao from './Components/Botao/';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <form>
+        <label>User: <input type="text"/></label>
+        <label>Password: <input type="password"/></label>
+      </form>
+      <button>Logar</button>
+      <button>Esqueci a Senha</button>
+      <Botao descricao='componente botao' nome='vasco'></Botao>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>
+          Vamos <code> Codar src/App.js</code> and save to reload.
+          </h1>
         </p>
         <a
           className="App-link"
@@ -15,9 +26,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Seja Bem Vindo ao Centro da Terra!!!
         </a>
-      </header>
+  </header>
     </div>
   );
 }
